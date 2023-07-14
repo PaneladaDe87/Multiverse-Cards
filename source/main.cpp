a69b8a1577266be6e45086802c636889f979ef1f
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-14 16:27:03 -0300

    Update lol

--- source/main.cpp
+++ source/main.cpp
@@ -1,1 +1,28 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
 
+class Cards {
+public:
+    std::string Name;
+    int Damage;
+    int Health;
+    sf::Texture Texture;
+    int Cost;
+
+    Card(const std::string& CardName, int CardDamage, int CardHealth, const std::string& CardTexture, int CardCost, std::string& CardType) {
+        : Name(CardName), Damage(CardDamage), Health(CardHealth), Cost(CardCost) {
+            if(!Texture.loadFromFile(CardTexture)) {
+                std::cout << "Error Code 0001";
+            }
+        }
+};
+
+int Main() {
+    Card Card1("Pirate", 63, 450, "./assets/card1.png", 4, "Trainer");
+    Card Card2("Indians", 33, 91, "./assets/card2.png", 2, "Troop");
+    Card Card3("Reaper", 50, 100, "./assets/card3.png", 3, "Troop");
+    Card Card4("Arrows", 95, 0, "./assets/card4.png", 4, "Spell");
+    Card Card5("Pelican", 100, 222, "./assets/card5.png", 5, "Troop");
+    Card Card5("Cannon", 72, 111, "./assets/card6.png", 3, "Building");
+}
