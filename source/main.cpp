a24ad677d1a6e4cacf1bb36f174c5a38af47021d
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-14 16:31:08 -0300

    Update 1

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-class Cards {
+class Card {
 public:
     std::string Name;
     int Damage;
@@ -11,18 +11,18 @@ public:
     int Cost;
 
     Card(const std::string& CardName, int CardDamage, int CardHealth, const std::string& CardTexture, int CardCost, std::string& CardType) {
-        : Name(CardName), Damage(CardDamage), Health(CardHealth), Cost(CardCost) {
-            if(!Texture.loadFromFile(CardTexture)) {
-                std::cout << "Error Code 0001";
-            }
+        Name(CardName), Damage(CardDamage), Health(CardHealth), Cost(CardCost) {
+        if(!Texture.loadFromFile(CardTexture)) {
+            std::cout << "Error Code 0001";
         }
+    }
 };
 
-int Main() {
+int main() {
     Card Card1("Pirate", 63, 450, "./assets/card1.png", 4, "Trainer");
     Card Card2("Indians", 33, 91, "./assets/card2.png", 2, "Troop");
     Card Card3("Reaper", 50, 100, "./assets/card3.png", 3, "Troop");
     Card Card4("Arrows", 95, 0, "./assets/card4.png", 4, "Spell");
     Card Card5("Pelican", 100, 222, "./assets/card5.png", 5, "Troop");
-    Card Card5("Cannon", 72, 111, "./assets/card6.png", 3, "Building");
+    Card Card6("Cannon", 72, 111, "./assets/card6.png", 3, "Building");
 }
