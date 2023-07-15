39fd06150483f5db44a163827088a42b50663498
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-14 23:17:29 -0300

    Balance changes

--- source/main.cpp
+++ source/main.cpp
@@ -19,11 +19,11 @@ public:
 };
 
 int main() {
-    Card Card1("Pirate", 63, 450, "./assets/card1.png", 4, "Trainer");
+    Card Card1("Pirate", 63, 290, "./assets/card1.png", 4, "Trainer");
     Card Card2("Indians", 33, 91, "./assets/card2.png", 2, "Troop");
-    Card Card3("Reaper", 50, 100, "./assets/card3.png", 3, "Troop");
+    Card Card3("Reaper", 50, 98, "./assets/card3.png", 3, "Troop");
     Card Card4("Arrows", 95, 0, "./assets/card4.png", 4, "Spell");
-    Card Card5("Pelican", 100, 222, "./assets/card5.png", 5, "Troop");
+    Card Card5("Pelican", 92, 151, "./assets/card5.png", 5, "Troop");
     Card Card6("Cannon", 72, 111, "./assets/card6.png", 3, "Building");
 
     return 0;
