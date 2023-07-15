35f4ab11ff945f460d5da2a663cac4a1c59d97e6
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-15 16:30:39 -0300

    Update?

--- source/main.cpp
+++ source/main.cpp
@@ -19,16 +19,17 @@ public:
 };
 
 int shit() {
-    // Spells literally cant have hp cuz this shit have no life, AAAAA >:(
+    // Spells literally cant have hp cuz this shit is invulnerable, AAAAA >:(
     Card Card1("Pirate", 63, 326, "./assets/card1.png", 4, "Trainer", false, false, 0.9);
     Card Card2("Indians", 40, 83, "./assets/card2.png", 2, "Troop", false, false, 1.1);
     Card Card3("Reaper", 57, 97, "./assets/card3.png", 3, "Troop", false, false, 1);
     Card Card4("Arrows", 76, 0, "./assets/card4.png", 3, "Spell", false, false, 1.25);
-    Card Card5("Pelican", 80, 151, "./assets/card5.png", 5, "Troop", true, true, 2);
+    Card Card5("Pelican", 76, 151, "./assets/card5.png", 5, "Troop", true, true, 2);
     Card Card6("Cannon", 72, 111, "./assets/card6.png", 3, "Building", false, false, 1);
     Card Card7("R.O.B.O.T", 100, 190, "./assets/card7.png", 6, "Troop", true, true, 1.8);
-    Card Card8("Fireball", 78, 0, "./assets/card8.png", 4, "Spell", false, false, 2.5);
+    Card Card8("Fireball", 80, 0, "./assets/card8.png", 4, "Spell", false, false, 2.5);
     Card Card9("Thief", 66, 97, "./assets/card9.png", 3, "Troop", true, false, 1.2);
+    Card Card10("Viking", 60, 99, "./assets/card10.png", 4, "Troop", false, false, 1.4);
 
     return 0;
 }
