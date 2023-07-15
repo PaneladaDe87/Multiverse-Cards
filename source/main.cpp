024b7573992c40d865f24b8266a1a4a77252c918
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-07-15 16:22:53 -0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -10,7 +10,7 @@ public:
     sf::Texture Texture;
     int Cost;
 
-    Card(const std::string& CardName, int CardDamage, int CardHealth, const std::string& CardTexture, int CardCost, std::string& CardType, bool WinCondition, bool AttackOnlyBuildings) :
+    Card(const std::string& CardName, int CardDamage, int CardHealth, const std::string& CardTexture, int CardCost, std::string& CardType, bool WinCondition, bool AttackOnlyBuildings, float AttackSpeed) :
         Name(CardName), Damage(CardDamage), Health(CardHealth), Cost(CardCost) {
         if(!Texture.loadFromFile(CardTexture)) {
             std::cout << "Error Code 0001";
@@ -20,15 +20,15 @@ public:
 
 int shit() {
     // Spells literally cant have hp cuz this shit have no life, AAAAA >:(
-    Card Card1("Pirate", 63, 326, "./assets/card1.png", 4, "Trainer", false, false);
-    Card Card2("Indians", 40, 83, "./assets/card2.png", 2, "Troop", false, false);
-    Card Card3("Reaper", 57, 97, "./assets/card3.png", 3, "Troop", false, false);
-    Card Card4("Arrows", 76, 0, "./assets/card4.png", 3, "Spell", false, false);
-    Card Card5("Pelican", 80, 151, "./assets/card5.png", 5, "Troop", true, true);
-    Card Card6("Cannon", 72, 111, "./assets/card6.png", 3, "Building", false, false);
-    Card Card7("R.O.B.O.T", 100, 190, "./assets/card7.png", 6, "Troop", true, true);
-    Card Card8("Fireball", 78, 0, "./assets/card8.png", 4, "Spell", false, false);
-    Card Card9("Thief", 66, 97, "./assets/card9.png", 3, "Troop", true, false);
+    Card Card1("Pirate", 63, 326, "./assets/card1.png", 4, "Trainer", false, false, 0.9);
+    Card Card2("Indians", 40, 83, "./assets/card2.png", 2, "Troop", false, false, 1.1);
+    Card Card3("Reaper", 57, 97, "./assets/card3.png", 3, "Troop", false, false, 1);
+    Card Card4("Arrows", 76, 0, "./assets/card4.png", 3, "Spell", false, false, 1.25);
+    Card Card5("Pelican", 80, 151, "./assets/card5.png", 5, "Troop", true, true, 2);
+    Card Card6("Cannon", 72, 111, "./assets/card6.png", 3, "Building", false, false, 1);
+    Card Card7("R.O.B.O.T", 100, 190, "./assets/card7.png", 6, "Troop", true, true, 1.8);
+    Card Card8("Fireball", 78, 0, "./assets/card8.png", 4, "Spell", false, false, 2.5);
+    Card Card9("Thief", 66, 97, "./assets/card9.png", 3, "Troop", true, false, 1.2);
 
     return 0;
 }
