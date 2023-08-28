3e4ecf4aa163f256d5e18586b19ef156578d8831
marcopereirajunior38@gmail.com <marcopereirajunior38@gmail.com>
2023-08-28 19:49:45 -0300

    SFML installed

--- source/main.cpp
+++ source/main.cpp
@@ -23,7 +23,7 @@ public:
     }
 };
 
-void shit() {
+void card() {
     // Spells literally cant have hp cuz this shit is invulnerable, AAAAA >:(
     // Every card starts on level 1
     Card Card1("Pirate", 96, 600, "One-By-One", "./assets/card1.png", 4, "Trainer", false, false, 0.9, 1, false, false);
@@ -34,7 +34,7 @@ void shit() {
     Card Card6("Cannon", 79, 666, "One-By-One", "./assets/card6.png", 3, "Building", false, false, 1, 1, false, false);
     Card Card7("R.O.B.O.T", 195, 870, "One-By-One", "./assets/card7.png", 7, "Troop", true, true, 2, 1, false, false);
     Card Card8("Fireball", 128, 0, "Explode", "./assets/card8.png", 4, "Spell", false, false, 2.5, 1, false, false);
-    Card Card9("Thief", 50, 444, "On-Collide", "./assets/card9.png", 3, "Troop", false, false, 1.2, 1, false, false);
+    Card Card9("Thief", 65, 444, "On-Collide", "./assets/card9.png", 3, "Troop", false, false, 1.2, 1, false, false);
     Card Card10("Normal Vikings", 70, 230, "One-By-One", "./assets/card10.png", 4, "Troop", false, false, 1.4, 4, false, false);
     Card Card11("Elite Vikings", 100, 460, "One-By-One", "./assets/card11.png", 6, "Troop", false, false, 1.7, 2, false, false);
     Card Card12("Rocket", 199, 0, "Explode", "./assets/card12.png", 5, "Spell", false, false, 3, 1, false, false);
@@ -47,4 +47,19 @@ void shit() {
     Card Card19("Combat Helicopter", 90, 330, "One-By-One", "./assets/card19.png", 4, false, false, 1.25, 1, true, false);
     Card Card20("Trojan Horse", 93, 300, "On-Collide", "./assets/card20.png", 4, true, true, 0.3, 1, true, false);
     Card Card21("Bomb Man", 96, 290, "Explode", "./assets/card21.png", 5, true, true, 1.8, 1, false, false);
+}
+
+void materializer() {
+    int Amount = 5;
+    int Timer = 0;
+    int TimerLimit = 280;
+    
+    while(Timer >= TimerLimit) {
+        Timer += 1;
+        
+        if(Timer >= TimerLimit) {
+            Amount += 1;
+            Timer = 0;
+        }
+    }
 }
